0a05c53d06ebc56a0189a6456269b0813d913306
mart <martddd@gmail.com>
2017-02-19 19:23:48 +0300

    Add function vk_groups_get

--- vk_requests.py
+++ vk_requests.py
@@ -1,4 +1,6 @@
 import requests
+from builtins import id
+
 import vk_access
 import json
 import json_open
@@ -12,12 +14,21 @@ TOKEN = json_open.setting()['user']['token']
 METHOD = 'messages.send'
 
 user_id = '14937004'
-user_id_msg = '131524133'
+user_id_msg = '14937004'
+# user_id_msg = '15606483'
 
-msg = '''
-это всё еще я)
-https://pp.vk.me/c322924/v322924100/25e0/RiacpvqUas4.jpg
-'''
+msg = 'Сообщение'
+
+
+def choice_method(id_method):
+    methods = {
+        "9" : "friends",
+        "11" : "groups.get",
+        "15" : "messages",
+    }
+    method = methods[id_method]
+    return method
+
 
 def vk_friends():
     url_request = '%s%s?user_id=%s&count=10&fields=name,bdate&order=hints&access_token=%s' % (API_URL, METHOD, user_id, TOKEN)
@@ -31,6 +42,14 @@ def vk_messages_send():
     r = requests.get(url_request)
     json_r = json.loads(r.text)
     return json_r
+
+
+def vk_groups_get(id_method):
+    url_request = '%s%s?user_id=%s&count=%d&access_token=%s&v=5.62' % (API_URL, choice_method('11'), user_id, 10, TOKEN)
+    r = requests.get(url_request)
+    json_r = json.loads(r.text)
+    return json_r['response']['items']
+
 
 def parse_json(json_list):
     list_friends = {}
@@ -55,4 +74,5 @@ def parse_json(json_list):
 
 # print(parse_json(vk_friends()))
 # print(vk_req())
-print(vk_messages_send())
+# print(vk_messages_send())
+print(vk_groups_get('11'))
